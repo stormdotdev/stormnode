50e17b54986cbaf658a46cfaa8bb065dd0712195
claudio <claudio@caporro.it>
2019-12-07 16:11:05 +0100

    first commit v0.1.0

---
+++ README.md
@@ -0,0 +1,57 @@
+<p align="center"><img src="https://user-images.githubusercontent.com/24509741/70376213-d149e300-1906-11ea-9497-c6505c798cdb.png"></p>
+
+# Stormclient
+![Version](https://img.shields.io/github/package-json/v/joinstormio/stormclient)  ![License](https://img.shields.io/github/license/joinstormio/stormclient)   [![Open Source Love](https://badges.frapsoft.com/os/v2/open-source.png?v=103)](https://github.com/ellerbrock/open-source-badges/)
+![Size](https://img.shields.io/github/size/joinstormio/stormclient/stormclient.js)
+NodeJs client for joinstorm node net
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+## Prerequisites
+
+ - You must have an account on joinstorm.io to access our network. Please register [here](https://joinstorm.io/register) to join our community
+ - NodeJs & npm
+
+## Installing
+
+clone this repo:
+> git clone https://github.com/joinstormio/stormclient.git
+
+> cd stormclient
+
+> npm install
+
+## Configuration
+
+### simple method
+> download the json configuration file from your client page [here](https://joinstorm.io/myclients) and put it in the same folder as stormclient.js
+
+### alternative
+create a stormclient.json file composed like this:
+```json
+{
+    "username": "client1",
+    "password": "password1",
+    "clientId": "user-client1-client1"
+}
+```
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/joinstormio/stormclient/tags).
+
+## Authors
+
+* **joinstorm.io** - [joinstorm.io](https://joinstorm.io)
+
+See also the list of [contributors](https://github.com/joinstormio/stormclient/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ stormclient.js
@@ -0,0 +1,239 @@
+#!/usr/bin/env node
+'use strict';
+
+const VERSION = '0.1.0';
+const yargs = require('yargs');
+
+const argv = yargs
+  .usage('$0 [ -c path-to-storm-client.json ]')
+  .help()
+  .version(VERSION)
+  .alias('h', 'help')
+  .default('c', __dirname + '/storm_client.json')
+  .alias('c', 'config')
+  .argv;
+
+const mqtt = require('mqtt');
+const http = require('http');
+const https = require('https');
+
+const NS_PER_SEC = 1e9;
+const MS_PER_NS = 1e6;
+
+const clientOptions = require(argv.config);
+const ownTopic = `joinstorm/clients/${clientOptions.clientId}/status`;
+
+const client  = mqtt.connect(process.env.STORM_CONNECT_URL || 'mqtts://joinstorm.io:8883', buildConnectOptions(clientOptions, ownTopic));
+let clientIp = null;
+
+client.on('connect', async function () {
+  clientIp = await sendHello();
+  client.subscribe('joinstorm/general');
+
+  client.publish(ownTopic, JSON.stringify(helloMessage()), {
+    retain: true
+  });
+});
+
+client.on('message', function (topic, message) {
+  let payload;
+
+  try {
+    payload = JSON.parse(message.toString());
+  } catch (err) {
+    payload = null;
+  }
+
+  if (!payload) {
+    return;
+  }
+
+  const command = payload.command;
+
+  switch (command) {
+    case 'flow':
+        handleNewFlow(payload, clientIp);
+      break;
+    default:
+      break;
+  }
+});
+
+async function handleNewFlow(flowConfig, clientIp) {
+  flowConfig.requests = flowConfig.requests.map(configFlowRequest);
+  const responsesData = [];
+
+  for (const config of flowConfig.requests) {
+    responsesData.push(await doRequest(config));
+  }
+
+  const result = {
+    clientIp: clientIp,
+    responsesData: responsesData
+  };
+
+  client.publish(`joinstorm/flows/${flowConfig.id}/${clientOptions.clientId}/results`, JSON.stringify(result));
+}
+
+function doRequest(config) {
+  return new Promise(function(resolve, reject) {
+    const responseData = {
+      requestId: config.id
+    };
+    const requestFn = config.protocol === 'http:' ? http.request : https.request;
+    const timings = newTimings();
+
+    const req = requestFn(config, function(res) {
+      res.once('readable', () => {
+        timings.firstByteAt = process.hrtime();
+
+        // do not remove this line
+        // https://github.com/nodejs/node/issues/21398
+        res.once('data', chunk => null);
+      });
+
+      res.setEncoding('utf8');
+
+      responseData.httpVersion = res.httpVersion;
+      responseData.headers = res.headers;
+      responseData.trailers = res.trailers;
+      responseData.statusCode = res.statusCode;
+      responseData.statusMessage = res.statusMessage;
+
+      const body = [];
+
+      if (config.includeBody) {
+        res.on('data', function(chunk) {
+          body.push(chunk);
+        });
+      }
+
+      res.once('end', function() {
+	      timings.endAt = process.hrtime();
+
+        if (config.includeBody) {
+          responseData.body = body.join('');
+        }
+
+        responseData.timings = timingsDone(timings);
+        resolve(responseData);
+      });
+    });
+
+    req.once('error', function (err) {
+      reject(err);
+    });
+
+    req.once('response', function (resp) {
+      responseData.localAddress = resp.socket.localAddress;
+      responseData.localPort = resp.socket.localPort;
+    });
+
+    req.on('socket', socket => {
+      socket.on('lookup', () => {
+        timings.dnsLookupAt = process.hrtime();
+      });
+      socket.on('connect', () => {
+        timings.tcpConnectionAt = process.hrtime();
+      });
+      socket.on('secureConnect', () => {
+        timings.tlsHandshakeAt = process.hrtime();
+      });
+    });
+
+    if (config.headers) {
+      if (!config.headers['Content-Length']) {
+        req.removeHeader('Content-Length');
+      }
+
+      if (!config.headers['Content-Type']) {
+        req.removeHeader('Content-Type');
+      }
+    }
+
+    req.end();
+  });
+}
+
+function configFlowRequest(requestConfig) {
+  return requestConfig;
+}
+
+/**
+* Get duration in milliseconds from process.hrtime()
+* @function getHrTimeDurationInMs
+* @param {Array} startTime - [seconds, nanoseconds]
+* @param {Array} endTime - [seconds, nanoseconds]
+* @return {Number} durationInMs
+* @author https://github.com/RisingStack/example-http-timings/blob/master/app.js
+*/
+function getHrTimeDurationInMs (startTime, endTime) {
+  const secondDiff = endTime[0] - startTime[0];
+  const nanoSecondDiff = endTime[1] - startTime[1];
+  const diffInNanoSecond = secondDiff * NS_PER_SEC + nanoSecondDiff;
+  return diffInNanoSecond / MS_PER_NS;
+}
+
+function newTimings() {
+  return {
+    startAt: process.hrtime(),
+    dnsLookupAt: null,
+    tcpConnectionAt: null,
+    tlsHandshakeAt: null,
+    firstByteAt: null,
+    endAt: null
+  };
+}
+
+function timingsDone(timings) {
+  const tlsHandshake = timings.tlsHandshakeAt !== null ? timings.tcpConnectionAt - timings.tlsHandshakeAt : null;
+
+  return {
+    dnsLookup: timings.dnsLookupAt !== null ? getHrTimeDurationInMs(timings.startAt, timings.dnsLookupAt) : null,
+    tcpConnection: getHrTimeDurationInMs(timings.dnsLookupAt || timings.startAt, timings.tcpConnectionAt),
+    tlsHandshake: timings.tlsHandshakeAt !== null ? getHrTimeDurationInMs(timings.tcpConnectionAt, timings.tlsHandshakeAt) : null,
+    firstByte: getHrTimeDurationInMs((timings.tlsHandshakeAt || timings.tcpConnectionAt), timings.firstByteAt),
+    contentTransfer: getHrTimeDurationInMs(timings.firstByteAt, timings.endAt),
+    total: getHrTimeDurationInMs(timings.startAt, timings.endAt)
+  };
+}
+
+function buildConnectOptions(clientOptions, ownTopic) {
+  return {
+    rejectUnauthorized: false, // until certificate fixed on server
+    username: clientOptions.username,
+    password: clientOptions.password,
+    clientId: process.env.STORM_CLIENTID || clientOptions.clientId,
+    protocolVersion: 5,
+    will: {
+      topic: ownTopic,
+      payload: null,
+      retain: true
+    }
+  };
+}
+
+function helloMessage(clientIp) {
+  return {
+    command: 'hello'
+  };
+}
+
+function sendHello() {
+  return new Promise(function(resolve, reject) {
+    https.get('https://joinstorm.io/api/hello?version='+VERSION+'&clientid='+clientOptions.clientId, resp => {
+      let data = '';
+
+      resp.on('data', chunk => {
+        data += chunk;
+      });
+
+      resp.on('end', () => {
+        resolve(data);
+      });
+    }).on('error', err => {
+      console.log(err.message);
+      reject(null);
+    });
+  });
+}
